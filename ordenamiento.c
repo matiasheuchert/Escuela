3c6d934f4e81be6d47b6ddec0abe9132843139fb
Matias Heuchert <73125441+matiasheuchert@users.noreply.github.com>
2021-10-28 23:45:54 -0300

    parcheado

--- ordenamiento.c
+++ ordenamiento.c
@@ -1,6 +1,6 @@
 #include <stdio.h>
 
-int vector[5], i, aux;
+int vector[5], i, j, aux;
 
 main(){
 	for(i = 0; i < 5; i++){
@@ -8,12 +8,14 @@ main(){
 	scanf("%d", &vector[i]);	
 	}
 	
+	for(j = 1; j < 5; j++){
 	for(i = 0; i < 4; i++){
 		if(vector[i] > vector[i+1]){
 		aux = vector[i];
 		vector[i] = vector[i+1];
 		vector[i+1] = aux;
 		}
+	}
 	}
 	for(i = 0; i < 5; i++){
 		printf("%d, ", vector[i]);
