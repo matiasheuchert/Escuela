2388a22cf78f962d09e9203d8f09d5679ca85e07
Matias Heuchert <73125441+matiasheuchert@users.noreply.github.com>
2021-10-29 00:03:31 -0300

    temporizador

--- ordenamiento.c
+++ ordenamiento.c
@@ -8,7 +8,7 @@ main(){
 	scanf("%d", &vector[i]);	
 	}
 	
-	for(j = 1; j < 5; j++){
+for(j = 1; j < 5; j++){
 	for(i = 0; i < 4; i++){
 		if(vector[i] > vector[i+1]){
 		aux = vector[i];
@@ -16,8 +16,15 @@ main(){
 		vector[i+1] = aux;
 		}
 	}
-	}
+}
+printf("Ordenando el vector... \n");
+printf("Usted obtendra los resultados en 3, ");
+sleep(1);
+printf("2, ");
+sleep(1);
+printf("1...\n");
+sleep(1);
 	for(i = 0; i < 5; i++){
-		printf("%d, ", vector[i]);
+		printf("%d | ", vector[i]);
 	}
 }
